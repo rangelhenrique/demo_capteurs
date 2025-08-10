f0eb4744ea8584d6b53b4d863c07805ebe09d577
Henrique Rangel <46088527-henriquerangelf@users.noreply.replit.com>
2025-08-10 12:03:13 +0000

    version_1

--- main.py
+++ main.py
@@ -7,7 +7,6 @@
 # étape 1 - Déclaration des libraries
 
 import csv
-from os import write
 import random
 import time
 from datetime import datetime
@@ -16,50 +15,47 @@ from datetime import datetime
 
 analyse_data = "donnees.csv"
 
-
 #étape 3 - Nommer les variables à prendre la mesure
 
-sortie = ["heure", "batterie(%)", "temperature(degrees)", "position_X", "position_Y"]
-
+sortie = [
+    "heure", "batterie(%)", "temperature(degrees)", "position_X", "position_Y"
+]
 
 #etape 4 - Création d'un fichier CSV
 with open(analyse_data, mode="w", newline="") as fichier_sortie:
-  writer = csv.writer(fichier_csv)
-  write.writerow(sortie)
+  writer = csv.writer(fichier_sortie)
+  writer.writerow(sortie)
 
 #etape 5 - Définir les valeurs aléatoires
 
-def gen_donees():
-  
- # Simulation des données
- 
 
-heure = datatime.now().strftime("%H:%M:%S %d/%m/%Y")
-betterie = random.randint(12.100)
-temperature = round(random.uniform(30.0, 60.0), 1)
-position_X = round(random.uniform(0.0, 60.0), 1)
-position_Y = round(random.uniform(0.0, 60.0), 1)
+def gen_donnees():
+  heure = datetime.now().strftime("%H:%M:%S %d-%m-%Y")
+  batterie = random.randint(12, 100)
+  temperature = round(random.uniform(30.0, 60.0), 1)
+  position_X = round(random.uniform(0.0, 60.0), 1)
+  position_Y = round(random.uniform(0.0, 60.0), 1)
+  return [heure, batterie, temperature, position_X, position_Y]
 
-return [heure, batterie, temperature, position_X, position_Y]
 
+#return [heure, batterie, temperature, position_X, position_Y]
 
 #etape 6 - Démarrer la simulation (nous allons prendre 10 mesures)
 
 for _ in range(10):
-  donees = gen_donees() #les donnees seront enregistrees dans la liste gen_donees
+  donnees = gen_donnees(
+  )  #les donnees seront enregistrees dans la liste gen_donees
 
+  #etape 7 - Enregistrer les données dans le fichier CSV
 
-#etape 7 - Enregistrer les données dans le fichier CSV
+  with open(analyse_data, mode="a", newline="") as fichier_sortie:
 
-with open(analyse_data, mode="a", newline="") as fichier_sortie:
+    writer = csv.writer(fichier_sortie)
+    writer.writerow(donnees)
 
-  writer = csv.writer(fichier_csv)
-  writer.writerow(donees)
+    #Etape 8 - Afficher les données dans le terminal
 
-#Etape 8 - Afficher les données dans le terminal
+    print("log: ", donnees)
 
-print("log: ", donees)
-
-#Etape 9 - Attendre 5 secondes avant de prendre la prochaine mesure
-time.sleep(5)
-
+    #Etape 9 - Attendre 5 secondes avant de prendre la prochaine mesure
+    time.sleep(1)
